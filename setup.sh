563e0f6fa718310057b5f38a6533874df2f7ca6d
Pau Riba <priba@cvc.uab.cat>
2019-11-12 11:18:54 +0100

    Files to install

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
+# Installation zsh
+sh zsh/install.sh
+
 # Installation Byobu
 sh byobu/install.sh
 
+# Installation NVIM
+sh nvim/install.sh
 
+gnome-session-quit
---
+++ zsh/install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# My prefered font
+sudo apt install -y fonts-firacode
+
+sudo apt-get -y update
+sudo apt-get -y install zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed -e 's/^\s*chsh -s/sudo chsh -s/g' -e 's/^\s*env\szsh.*$/#/g')"
+
+cd
+ln -s -f .config/priba_setup/zsh/zshrc .zshrc
+mv .oh-my-zsh .config/priba_setup/zsh/oh-my-zsh
+cd ~/.config/priba_setup/zsh/oh-my-zsh/plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting
+cd ~/.config/priba_setup/zsh/
+cp myagnoster.zsh-theme ~/.config/priba_setup/zsh/oh-my-zsh/themes/
+chsh -s `which zsh`
